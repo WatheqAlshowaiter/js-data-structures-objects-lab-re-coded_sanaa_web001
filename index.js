a15c943338b4fb2574f9b182a166c5ab7c6f3cf6
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-16 19:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  return (driver[key] = value);
+  return driver[key] = value;
 }
 
 function deleteFromDriverByKey(driver, key) {
